d87f3535277632fa52bbeec0a729db14aa81e65f
nawawishkid <nawawish.s@acuratech.io>
2023-06-21 20:36:29 +0700

    Add support for Shopee

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 const LazadaApiClient = require("./lazada");
+const ShopeeApiClient = require("./shopee");
 const app = express();
 const lazadaClient = new LazadaApiClient(
   process.env.LAZADA_APP_KEY,
   process.env.LAZADA_APP_SECRET,
   "THAILAND"
+);
+const shopeeClient = new ShopeeApiClient(
+  process.env.SHOPEE_PARTNER_ID,
+  process.env.SHOPEE_PARTNER_KEY,
+  process.env.SHOPEE_IS_SANDBOX
 );
 
 app.get("/", (req, res) => {
@@ -26,6 +32,10 @@ oauthRouter.get("/url/:platform", (req, res) => {
   switch (platform) {
     case "lazada":
       authUrl = lazadaClient.getAuthUrl(process.env.HOSTNAME);
+      break;
+
+    case "shopee":
+      authUrl = shopeeClient.getAuthUrl(process.env.HOSTNAME);
       break;
 
     default:
@@ -50,7 +60,7 @@ oauthRouter.get("/callback/:platform", async (req, res) => {
   console.log(`Received ${platform} authorization callback`);
 
   switch (platform) {
-    case "lazada":
+    case "lazada": {
       const { code } = req.query;
 
       if (!code) {
@@ -81,6 +91,51 @@ oauthRouter.get("/callback/:platform", async (req, res) => {
         });
       }
       break;
+    }
+
+    case "shopee": {
+      const { code, shop_id } = req.query;
+
+      if (!code) {
+        msg = "'code' query parameter is required";
+
+        return res.status(400).json({
+          success: false,
+          msg,
+          error: msg,
+        });
+      }
+
+      if (!shop_id) {
+        msg = "'shop_id' query parameter is required";
+
+        return res.status(400).json({
+          success: false,
+          msg,
+          error: msg,
+        });
+      }
+
+      console.log("code: ", code);
+      console.log("shop_id: ", shop_id);
+
+      try {
+        console.log("Exchanging code for tokens...");
+
+        response = await shopeeClient.exchangeCodeForTokens(code, shop_id);
+      } catch (e) {
+        msg = "Failed to get Shopee tokens: " + e.message;
+
+        console.error(msg);
+
+        return res.status(500).json({
+          success: false,
+          msg,
+          error: msg,
+        });
+      }
+      break;
+    }
 
     default:
       msg = `Unsupported platform: '${platform}'`;
---
+++ shopee.js
@@ -0,0 +1,68 @@
+const crypto = require("crypto");
+
+class ShopeeApiClient {
+  _partnerId = undefined;
+  _partnerKey = undefined;
+  _isSandbox = undefined;
+
+  constructor(partnerId, partnerKey, isSandbox) {
+    this._partnerId = parseInt(partnerId);
+    this._partnerKey = partnerKey;
+    this._isSandbox = !!isSandbox;
+  }
+
+  getAuthUrl(hostname) {
+    const authUrl = new URL(`${this.getBaseUrl()}/api/v2/shop/auth_partner`);
+    const timestamp = this.getCurrentTimestamp();
+
+    authUrl.searchParams.set("partner_id", this._partnerId);
+    authUrl.searchParams.set("timestamp", timestamp);
+    authUrl.searchParams.set(
+      "sign",
+      this.getSignature(authUrl.pathname, timestamp)
+    );
+    authUrl.searchParams.set(
+      "redirect",
+      new URL(`https://${hostname}/oauth/callback/shopee`).toString()
+    );
+
+    return authUrl.toString();
+  }
+
+  exchangeCodeForTokens(code, shopId) {
+    const url = new URL(`${this.getBaseUrl()}/api/v2/auth/token/get`);
+    const timestamp = this.getCurrentTimestamp();
+
+    url.searchParams.set("sign", this.getSignature(url.pathname, timestamp));
+    url.searchParams.set("timestamp", timestamp);
+    url.searchParams.set("partner_id", this._partnerId);
+
+    return fetch(url.toString(), {
+      method: "POST",
+      body: JSON.stringify({
+        code,
+        shop_id: parseInt(shopId),
+        partner_id: this._partnerId,
+      }),
+    }).then((res) => res.json());
+  }
+
+  getSignature(path, timestamp) {
+    return crypto
+      .createHmac("sha256", this._partnerKey)
+      .update(`${this._partnerId}${path}${timestamp}`)
+      .digest("hex");
+  }
+
+  getCurrentTimestamp() {
+    return Date.now() / 1000;
+  }
+
+  getBaseUrl() {
+    return this._isSandbox
+      ? "https://partner.test-stable.shopeemobile.com"
+      : "https://partner.shopeemobile.com";
+  }
+}
+
+module.exports = ShopeeApiClient;
